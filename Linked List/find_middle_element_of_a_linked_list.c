560d88c1e61cd44abeebbc58fe5045cf78523aad
Ramji-Chauhan <97727977+Ramji-Chauhan@users.noreply.github.com>
2023-08-22 06:40:16 +0530

    Add files via upload

---
+++ Linked List/find_middle_element_of_a_linked_list.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+    int info;
+    struct node *next;
+}nodetype;
+
+int getMiddle(nodetype*);
+
+int main(){
+    char ch;
+    nodetype *head=NULL;
+    do{
+        nodetype*p;
+        p=(nodetype*)malloc(sizeof(nodetype*));
+        if(p==NULL){
+            printf("Not nough Memory\n");
+        }
+        else{
+            int val;
+            printf("Enter value to push in linked list\n");
+            scanf("%d",&val);
+            p->info=val;
+            p->next=head;
+            head=p;
+        }
+        printf("Do you want to push\n");
+        scanf(" %c",&ch);
+    }
+    while(ch=='y');
+    int ans;
+    ans=getMiddle(head);
+    printf("%d",ans);
+
+     while (head != NULL) {
+        nodetype *temp = head;
+        head = head->next;
+        free(temp);
+    }
+    return 0;
+}
+
+int getMiddle(nodetype*hd){
+    nodetype*z=hd;
+    int counter=0;
+    while(z){
+        z=z->next;
+        counter++;
+    }
+    z=hd;
+    for(int i=0;i<counter/2;i++){
+        z=z->next;
+    }
+    int ans;
+    ans=z->info;
+    return ans;
+}
+
